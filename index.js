4913c4f8f2b7c661228787eb45124865ecba3d2d
Mikhail Mezhibovskiy <mikhail.mezh@icloud.com>
2025-06-09 00:19:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const commits = [] // [x, y, count]
+const commits = [[1, 0, 3], [0, 1, 1], [1, 1, 4]] // [x, y, count]
 const markCommit = (x, y) => {
     const date = moment()
         .startOf('y')
-        .subtract(8, 'y')
+        .subtract(1, 'y')
         .add(x, 'w')
         .add(y, 'd')
         .format();
@@ -17,13 +17,13 @@ const markCommit = (x, y) => {
     };
 
     jsonfile.writeFile(path, data, ()=>{
-    simpleGit().add([path]).commit(date, {'--date': date}).push();
+        simpleGit().add([path]).commit(date, {'--date': date});
     });
 }
 
-for (let i = 0; i < commits.length; i ++) {
-    const commit = commits[i]
-    for (let j = 0; j < commit[2]; j ++) {
-        markCommit(commit[0], commit[1])
+for (const [x, y, count] of commits) {
+    for (let i = 0; i < count; i++) {
+        markCommit(x, y)
     }
 }
+simpleGit().push();
