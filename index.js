1d0fc6757bbc99b00185b6a860784336cc7edd4a
Mikhail Mezhibovskiy <mikhail.mezh@icloud.com>
2025-06-08 23:25:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-
+const commits = [] // [x, y, count]
 const markCommit = (x, y) => {
     const date = moment()
         .startOf('y')
-        .subtract(10, 'y')
+        .subtract(8, 'y')
         .add(x, 'w')
         .add(y, 'd')
         .format();
@@ -21,4 +21,9 @@ const markCommit = (x, y) => {
     });
 }
 
-markCommit(0, 0);
+for (let i = 0; i < commits.length; i ++) {
+    const commit = commits[i]
+    for (let j = 0; j < commit[2]; j ++) {
+        markCommit(commit[0], commit[1])
+    }
+}
